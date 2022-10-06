3b46d1344bca7f850a435738065af13e18c4594e
Daniel Rodriguez <maieutiquer@gmail.com>
2022-10-06 22:42:16 +0300

    chore: fix lint errors and formatting, cleanup cra examples

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+import { store } from './app/store'
+import { App } from './App'
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+test('renders Metamask text', () => {
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    )
+
+    expect(screen.getByText(/Metamask/i)).toBeInTheDocument()
+})
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,12 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit'
+import type { ThunkAction, Action } from '@reduxjs/toolkit'
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+    reducer: {
+        test: (state = {}, action) => state,
+    },
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>
