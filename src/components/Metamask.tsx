9bb5fffc0662490ed0667022d505c48d692dd84c
Daniel Rodriguez <maieutiquer@gmail.com>
2022-10-11 16:19:40 +0300

    feat: get all user tokens and display their details

--- src/components/Metamask.tsx
+++ src/components/Metamask.tsx
@@ -1,14 +1,66 @@
 import { useCallback, useEffect, useState } from 'react'
+import type { FC } from 'react'
 import { Button } from '@mui/material'
 import { ethers } from 'ethers'
+import type { providers } from 'ethers'
+
 import genericErc20Abi from '../erc20.json'
 
 const NEXO_CONTRACT_ADDRESS = '0xb62132e35a6c13ee1ee0f84dc5d40bad8d815206'
 
-export const Metamask = () => {
+type tokenDescription = {
+    address: string
+    decimals: number
+    name: string
+    symbol: string
+}
+
+type tokenWithDetails = tokenDescription & {
+    balance: string
+    totalSupply: string
+}
+
+const getBalanceByTokenContract = async ({
+    contractAddress,
+    decimals,
+    provider,
+    account,
+}: {
+    contractAddress: string
+    decimals: number
+    provider: providers.Web3Provider
+    account?: string
+}) => {
+    const nexoContract = new ethers.Contract(contractAddress, genericErc20Abi, provider)
+    const nexoBalance = (
+        account != null ? await nexoContract.balanceOf(account) : await nexoContract.totalSupply()
+    ).toString()
+    const balanceAsBigNumber = ethers.utils.parseUnits(nexoBalance, 0)
+
+    return ethers.utils.formatUnits(balanceAsBigNumber, decimals)
+}
+
+// const getTotalSupplyOfToken = async ({
+//     contractAddress,
+//     decimals,
+//     provider,
+// }: {
+//     contractAddress: string
+//     decimals: number
+//     provider: providers.Web3Provider
+// }) => {
+//     const nexoContract = new ethers.Contract(contractAddress, genericErc20Abi, provider)
+//     const nexoBalance = (await nexoContract.totalSupply()).toString()
+//     const balanceAsBigNumber = ethers.utils.parseUnits(nexoBalance, 0)
+
+//     return ethers.utils.formatUnits(balanceAsBigNumber, decimals)
+// }
+
+export const Metamask: FC = () => {
     const [selectedAddress, setSelectedAddress] = useState(null)
-    const [balance, setBalance] = useState('')
+    const [ethBalance, setEthBalance] = useState('')
     const [nexoTokens, setNexoTokens] = useState('')
+    const [allTokens, setAllTokens] = useState([])
 
     const onConnectButtonClick = useCallback(async () => {
         const provider = new ethers.providers.Web3Provider(window.ethereum)
@@ -18,12 +70,67 @@ export const Metamask = () => {
 
         const accountBalance = await provider.getBalance(account)
         const balanceInEther = ethers.utils.formatEther(accountBalance)
-        setBalance(balanceInEther)
+        setEthBalance(balanceInEther)
 
-        const nexoContract = new ethers.Contract(NEXO_CONTRACT_ADDRESS, genericErc20Abi, provider)
-        const nexoBalance = (await nexoContract.balanceOf(account)).toString()
-        const balanceAsBigNumber = ethers.utils.parseUnits(nexoBalance, 0)
-        setNexoTokens(ethers.utils.formatUnits(balanceAsBigNumber, 18))
+        setNexoTokens(
+            await getBalanceByTokenContract({
+                contractAddress: NEXO_CONTRACT_ADDRESS,
+                decimals: 18,
+                provider,
+                account,
+            })
+        )
+
+        const allTokenDescriptions = await fetch(
+            'https://gateway.ipfs.io/ipfs/QmdvdRrtqxbBKmCpxFyraXEvHPGKkenkZKWGBD4KkLwXCw'
+        )
+        // nexo: 1791
+        const allTokenDescriptionsJson: tokenDescription[] = await allTokenDescriptions.json()
+
+        const tokensToCheck = allTokenDescriptionsJson.map(({ address, decimals, name, symbol }) =>
+            (async () => {
+                try {
+                    return {
+                        name,
+                        symbol,
+                        decimals,
+                        address,
+                        balance: await getBalanceByTokenContract({
+                            contractAddress: address,
+                            decimals,
+                            provider,
+                            account,
+                        }),
+                        totalSupply: await getBalanceByTokenContract({
+                            contractAddress: address,
+                            decimals,
+                            provider,
+                        }),
+                    }
+                } catch (error) {
+                    console.error(error)
+                }
+            })()
+        )
+
+        const allTokensChecked = await Promise.allSettled(tokensToCheck)
+
+        // TODO: check ts error: Type 'any' is not assignable to type 'never'.
+        setAllTokens(
+            // @ts-ignore
+            allTokensChecked
+                // .filter(
+                //     ({ status, value: { balance: retrievedBalance } = {} }) =>
+                //         status === 'fulfilled' && retrievedBalance !== '0.0'
+                // )
+
+                .filter((x): x is PromiseFulfilledResult<tokenWithDetails> => x.status === 'fulfilled')
+                .map(({ value }) => value)
+                .filter(Boolean)
+                .filter(({ balance: retrievedBalance }) => retrievedBalance !== '0.0' && retrievedBalance !== '0')
+        )
+
+        // setAllContracts(allContractAddressesJson)
     }, [])
 
     const handleAccountsChanged = (accounts: any) => {
@@ -39,6 +146,8 @@ export const Metamask = () => {
             window.ethereum.removeListener('accountsChanged', handleAccountsChanged)
         }
     }, [])
+
+    console.log(allTokens)
 
     return (
         <div>
@@ -61,10 +170,36 @@ export const Metamask = () => {
                         {/* TODO: logos */}
                         {/* TODO: format thousands with commas */}
                         {/* TODO: loaders */}
-                        Your ETH Balance: <strong>{balance}</strong>
+                        Your ETH Balance: <strong>{ethBalance}</strong>
                     </p>
                     <p>
                         Your NEXO tokens: <strong>{nexoTokens}</strong>
+                    </p>
+                    <p>
+                        All tokens that you have:{' '}
+                        {allTokens.map(({ name, balance, symbol, decimals, totalSupply }) => (
+                            <details>
+                                <summary>
+                                    {name}: {balance}
+                                </summary>
+                                <p>
+                                    <dl>
+                                        <dt>
+                                            <strong>Symbol:</strong>
+                                        </dt>
+                                        <dd>{symbol}</dd>
+                                        <dt>
+                                            <strong>Decimals:</strong>
+                                        </dt>
+                                        <dd>{decimals}</dd>
+                                        <dt>
+                                            <strong>Total Supply:</strong>
+                                        </dt>
+                                        <dd>{totalSupply}</dd>
+                                    </dl>
+                                </p>
+                            </details>
+                        ))}
                     </p>
                 </div>
             )}
